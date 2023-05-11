2318e1fade8ba2e55deb8b0c37b825f52dd1bf69
Will0410 <williamcjunior460@gmail.com>
2023-05-11 18:05:12 -0300

    Atv Crud

---
+++ Crud/resources/views/estudante/create.blade.php
@@ -0,0 +1,22 @@
+@extends('students.frame')
+@section('content')
+  
+<div class="card" style="margin:20px;">
+  <div class="card-header">Criação de novos estudantes</div>
+  <div class="card-body">
+       
+      <form action="{{ url('/estudantes') }}" method="post">
+        {!! csrf_field() !!}
+        <label>Name</label></br>
+        <input type="text" name="name" id="name" class="form-control"></br>
+        <label>Email</label></br>
+        <input type="text" name="email" id="email" class="form-control"></br>
+        <label>Phone</label></br>
+        <input type="text" name="phone" id="phone" class="form-control"></br>
+        <input type="submit" value="Save" class="btn btn-success"></br>
+    </form>
+    
+  </div>
+</div>
+  
+@stop
---
+++ Crud/resources/views/estudante/show.blade.php
@@ -0,0 +1,14 @@
+@extends('students.frame')
+@section('content')
+  
+<div class="card" style="margin:20px;">
+  <div class="card-header">DEscrição dos estudantes</div>
+  <div class="card-body">
+        <div class="card-body">
+        <h5 class="card-title">Name : {{ $students->name }}</h5>
+        <p class="card-text">Endereço : {{ $students->email }}</p>
+        <p class="card-text">Phone : {{ $students->phone }}</p>
+  </div>
+    </hr>
+  </div>
+</div>
